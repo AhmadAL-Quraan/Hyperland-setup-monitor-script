0eca214feb297f0293872411b560b3febf282bf8
Ahmad AL-Quraan <ahmadquraan.git@gmail.com>
2026-03-21 20:32:16 +0300

    Cache options in tmp directory and improve the qulaity of interacting with user

--- monitor.sh
+++ monitor.sh
@@ -23,9 +23,75 @@
 
 #   * Smarter "best mode" (balance resolution + refresh rate).
 
+RED="\e[31m"
+BLUE="\e[34m"
+RESET="\e[0m"
+
+# All availabe mode options for the monitor connected via HDMI
 value=0 #postion value (right, left, up, down)
 Hirtz=0
-Dimensions=0
+Dimensions=0 #width x height
+
+# CHeck if there is previous answer for modes or settings applied
+
+mirroring() {
+  while true; do
+    read -p "Mirror / seperate ? (m/s) " mirror
+    case "$mirror" in
+    m | mirror)
+      hyprctl keyword monitor HDMI-A-3,"$Dimensions"@"$Hirtz","$value",1,mirror,eDP-1
+      exit
+      ;;
+    s | seperate)
+      break
+      ;;
+    *)
+      echo "Enter valid answer (m/s) !!"
+      ;;
+    esac
+
+  done
+
+}
+
+# Start of the script
+previous_settings="/tmp/hdmi_script/previous_settings"
+mode_file="/tmp/hdmi_script/HDMI_modes"
+
+if [[ -f "$previous_settings" ]]; then
+  echo -e "Do you want to apply the last settings ? \n"
+  echo -e "$(cat $previous_settings)\n"
+
+  while true; do
+
+    read -p "(y/n): " apply
+    case "$apply" in
+    y | yes)
+      Dimensions=$(cat /tmp/hdmi_script/previous_settings | awk '{print $2}')
+      Hirtz=$(cat /tmp/hdmi_script/previous_settings | awk '{print $4}')
+      value=$(cat /tmp/hdmi_script/previous_settings | awk '{print $6}')
+      mirroring
+
+      hyprctl keyword monitor HDMI-A-3,"$Dimensions"@"$Hirtz","$value",1
+      exit
+      ;;
+    n | no)
+      break
+      ;;
+    *)
+      echo "Enter valid answer (y/n) !!"
+      ;;
+
+    esac
+  done
+fi
+
+if [[ ! -f "$mode_file" ]]; then
+  all_option=$(hyprctl monitors -j | jq '.[] | select(.name=="HDMI-A-3") | .availableModes[] ' | sed 's/"//g' | sort -t'x' -k1,1 -n | nl -w1 -s ') ')
+  mkdir "/tmp/hdmi_script/" 2>/dev/null
+  echo "$all_option" >"$mode_file"
+
+fi
 
 function choose_position() {
   #Loop to apply valid postion
@@ -48,10 +114,7 @@ function choose_position() {
       value=0x1080
       break
       ;;
-    *)
-      echo "Enter valid input right/left/up/down"
-      ;;
-    esac
+    *) echo "Enter valid input right/left/up/down" ;; esac
   done
 }
 
@@ -78,17 +141,17 @@ best_mode() {
 
   echo "✅ Best mode selected: $best"
 
-  dimensions=$(echo "$best" | cut -d'@' -f1)
+  Dimensions=$(echo "$best" | cut -d'@' -f1)
   hirtz=$(echo "$best" | cut -d'@' -f2 | sed 's/Hz//')
 
   choose_position
 
-  hyprctl keyword monitor HDMI-A-3,"$dimensions"@"$hirtz","$value",1
+  hyprctl keyword monitor HDMI-A-3,"$Dimensions"@"$hirtz","$value",1
   exit
 }
 
 all_options() {
-  hyprctl monitors -j | jq '.[] | select(.name=="HDMI-A-3") | .availableModes[] ' | sed 's/"//g' | sort -t'x' -k1,1 -n | nl -w1 -s ') '
+  cat "$mode_file"
 }
 
 specific_option() {
@@ -96,7 +159,7 @@ specific_option() {
     # 🔹 Validate input
     while true; do
       read -p "Enter specific hz: " HZ
-      if [[ ! "$HZ" =~ ^[0-9]+$ || "$HZ" -lt 20 || "$HZ" -gt 540 ]]; then
+      if [[ ! "$HZ" =~ ^[0-9]+$ || "$HZ" -lt 30 || "$HZ" -gt 540 ]]; then
         echo "Enter valid number !!"
       else
         break
@@ -128,21 +191,40 @@ specific_option() {
 
     break
   done
+
+  while true; do
+    read -p "Continue configuration with the $HZ hirtz (you only need to choose dimension) ? (yes/no) " continue
+    case "$continue" in
+    y | yes)
+      Hirtz="$HZ"
+      break
+      ;;
+    n | no)
+      break
+      ;;
+    *)
+      echo "Enter valid answer (yes/no) !!"
+      ;;
+    esac
+
+  done
+
 }
 
 available_modes() {
   while true; do
-    read -p "Show available modes? (y/n): " option
+    read -p "Show available dimension @ hirtz ? (y/n): " option
     option=$(echo "$option" | tr '[:upper:]' '[:lower:]')
 
     case "$option" in
     y | yes)
       while true; do
-        read -p "All options (y) or specific (n) or best (b) -> based on highest HZ ? " option2
+        read -p "All modes (y) or specific hirtz option lookup (n) or best mode (based on hirtz) (b) ? " option2
         option2=$(echo "$option2" | tr '[:upper:]' '[:lower:]')
 
         case "$option2" in
         y | yes)
+          echo -e "${BLUE}Note! : available modes sorted based on width from loweset to heighst (width x height @ hirtz)${RESET}"
           all_options
           break
           ;;
@@ -172,16 +254,18 @@ available_modes() {
 }
 
 choose_hirtz_and_dimensions() {
-  while true; do
-    read -p "Enter the hz for the screen, Ex. 60 for 60hz: " hirtz
-    if [[ -n "$hirtz" ]]; then
-      Hirtz="$hirtz"
-      break
-    fi
-    echo "Hirtz can't be empty!!"
-  done
+  echo -e "\n"
+  if [[ "$Hirtz" == 0 ]]; then
 
-  echo "You can treat all the hz appeared as a one value (the value you choosen) and you don't need to specify."
+    while true; do
+      read -p "Enter the hz for the screen, Ex. 60 for 60hz: " hirtz
+      if [[ -n "$hirtz" ]]; then
+        Hirtz="$hirtz"
+        break
+      fi
+      echo "Hirtz can't be empty!!"
+    done
+  fi
   while true; do
     read -p "Enter the Dimensions for the screen, Ex. 1920x1080: " dimensions
     if [[ -n "$dimensions" ]]; then
@@ -192,11 +276,14 @@ choose_hirtz_and_dimensions() {
 
   done
 }
+
 choose_settings() {
   choose_hirtz_and_dimensions
   choose_position
+  mirroring
 
   hyprctl keyword monitor HDMI-A-3,"$Dimensions"@"$Hirtz","$value",1
+  echo "Dimension: $Dimensions       Hirtz: $Hirtz       Postion: $value" >"$previous_settings"
 }
 
 available_modes
